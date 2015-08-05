423dbf18c76c9c0f7eec5dac4c852313b55a8795
MKS-students <mks.students@hackreactor.com>
2015-08-05 17:30:27 -0500

    Explored rook solver solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,55 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
+  var rookSetter = function(index, doNoUseArray){
+    //loop through doNotUseArray
+    //do the check (if index !== doNoUseArray)
+    //var row = solution.get(index)
+
+
+    for(var i = 0; i < n; i++){
+      var row = solution.get(i);
+      for(var j = 0; j < n; j++){
+        row[j] = 1;
+      }
+    }
+  }
+
+    /*
+      var rookSetter = function(arr, doNotuseIndexarr){
+          //loop through the arr
+          //if the index is not in the doNotUseArr
+               //set the value at the position to 1
+               // add used index to doNoUseArray
+          //rookSetter(arr,doNotUseIndexArray)
+      }
+
+
+          for(var i=0; i<n; i++){
+             if(_.contains(doNotUseArray, i) === -1){
+                for(var j = 0; j < n; j++){
+                  if(_.contains(doNotuseArray, j) === -1){
+                    var row = solution.get(i);
+                    row[j] = 1;
+                    doNotUseArray.push(j);
+                  }
+                }
+             }
+             check if the nearest positions are ones?
+             rookSetter(arr, doNotUseArray);
+          }
+
+    */
+
+
+    // put the rooks in systematically
+    // after the correct # of rooks are placed
+    //   check the rows/columns
+    //   if there are no conflicts
+    //     then return the board
+  }
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
